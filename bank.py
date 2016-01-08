20a457b3905f79e8fb8a337ffb5910e9c69faa03
proto3 <lucas@sogilis.com>
2016-01-08 13:51:19 +0100

    create basic object for the account collection

---
+++ bank.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+class Date:
+    def __init__(self, day, month, year):
+        self.date = datetime(year, month, day)
+
+class Money:
+    def __init__(self, amount):
+        self.amount = amount
+
+class Account:
+    def __init__(self, content, opening_date):
+        self.content = content
+        self.opening_date = opening_date
+
+class AccountCollection:
+    def __init__(self):
+        self.content = list()
+
+class Bank:
+    def __init__ (self):
+        self.account_list = list()
+
+
+print "hello bank"
